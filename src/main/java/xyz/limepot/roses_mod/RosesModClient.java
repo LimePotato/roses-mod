17779e4591b19645441c2d7bdf71f449ccbc176f
LimePot <nelle@limepot.xyz>
2023-01-07 01:14:25 -0700

    Update RosesModClient.java

--- src/main/java/xyz/limepot/roses_mod/RosesModClient.java
+++ src/main/java/xyz/limepot/roses_mod/RosesModClient.java
@@ -6,7 +6,7 @@ import net.minecraft.client.render.RenderLayer;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
-public abstract class RosesModClient implements ClientModInitializer {
+public class RosesModClient implements ClientModInitializer {
 	public void onInitializeClient(ModContainer mod) {
 		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), RosesMod.POTTED_ROSE);
 		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), RosesMod.ROSE_FLOWER);
