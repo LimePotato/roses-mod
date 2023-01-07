fa6bfb217f65cc26c6f5d8907b73c3a6b2b15b87
LimePot <nelle@limepot.xyz>
2023-01-07 00:56:37 -0700

    ????

--- src/main/java/xyz/limepot/roses_mod/RosesModClient.java
+++ src/main/java/xyz/limepot/roses_mod/RosesModClient.java
@@ -7,9 +7,6 @@ import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public abstract class RosesModClient implements ClientModInitializer {
-
-
-	@Override
 	public void onInitializeClient(ModContainer mod) {
 		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), RosesMod.POTTED_ROSE);
 		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), RosesMod.ROSE_FLOWER);
